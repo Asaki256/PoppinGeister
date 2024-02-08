28541197ccb6cb392ac9842abd1c8f43603b8824
Masaki <77131849+Asaki256@users.noreply.github.com>
2024-02-08 12:32:57 +0900

    ユーザ設定_アイコン選択時の処理追加

--- Assets/Scripts/Title/UserSettingManager.cs
+++ Assets/Scripts/Title/UserSettingManager.cs
@@ -9,13 +9,14 @@ public class UserSettingManager : MonoBehaviour
 {
 
     [SerializeField] ToggleGroup colorToggleGroup;
-    // Start is called before the first frame update
+    [SerializeField] ToggleGroup iconToggleGroup;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
 
     // トグル選択時にカラーコード取得
     public void onClickColorToggle()
@@ -27,7 +28,23 @@ public class UserSettingManager : MonoBehaviour
             Toggle selectedColorToggle = colorToggleGroup.ActiveToggles().FirstOrDefault();
             if(selectedColorToggle.isOn && selectedColorToggle){
                 GameObject colorCircle = selectedColorToggle.transform.Find("IsSelectCircle/BGCircle/ColorCircle").gameObject;
+                // Color型からカラーコードへ変換
                 string colorCodeStr = ColorUtility.ToHtmlStringRGB(colorCircle.GetComponent<Image>().color);
+            }
+        }
+    }
+
+    // トグル選択時にアイコン番号取得
+    public void onClickIconToggle()
+    {
+        // Toggleの仕様なのか、2回呼び出しがされる。
+        // ラジオボタンOn側とOff側の双方から呼び出されてるぽい。
+        // 変数を更新後、戻るボタン押下時にまとめて保存するため、パフォーマンス影響は小
+        if(colorToggleGroup.AnyTogglesOn()){
+            Toggle selectedIconToggle = iconToggleGroup.ActiveToggles().FirstOrDefault();
+            if(selectedIconToggle.isOn && selectedIconToggle){
+                string iconName = selectedIconToggle.gameObject.name;
+                Debug.Log(iconName);
             }
         }
     }
